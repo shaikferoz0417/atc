b1372e966e97b8b7c7eb1c96be316721f67d5360
Feroz <shaikferozhussain7664@gmail.com>
2024-07-11 01:30:57 +0300

    UI changes

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,49 +1,63 @@
-sap.ui.define([], () => {
+sap.ui.define(
+  [],
+  () => {
     "use strict";
     return {
-        statusState(sState) {
-            switch (sState) {
-                case 'Approved Pending':
-                    return 'Indication03';
-                case 'Approved':
-                    return 'Success';
-                case 'Rejected':
-                    return 'Error';
-                default:
-                    return 'None';
-            }
-        },
+      statusState(sState) {
+        switch (sState) {
+          case "Approved Pending":
+            return "Indication03";
+          case "Approved":
+            return "Success";
+          case "Rejected":
+            return "Error";
+          case "Part Paid":
+            return "Indication03";
+          case "Paid":
+            return "Success";
+          case "Unpaid":
+            return "Error";
+          default:
+            return "None";
+        }
+      },
 
-        statusIcon(sState) {
+      statusIcon(sState) {
+        switch (sState) {
+          case "Approved Pending":
+            return "sap-icon://alert";
+          case "Approved":
+            return "sap-icon://sys-enter-2";
+          case "Rejected":
+            return "sap-icon://error";
+          case "Part Paid":
+            return "sap-icon://alert";
+          case "Paid":
+            return "sap-icon://sys-enter-2";
+          case "Unpaid":
+            return "sap-icon://error";
+          default:
+            return "None";
+        }
+      },
 
-            switch (sState) {
-                case 'Approved Pending':
-                    return 'sap-icon://alert';
-                case 'Approved':
-                    return 'sap-icon://sys-enter-2';
-                case 'Rejected':
-                    return 'sap-icon://error';
-                default:
-                    return 'None';
-            }
-        },
-
-        ComplaintStatusState(sState) {
-            switch (sState) {
-                case 'Open':
-                    return 'Information';
-                case 'Closed':
-                    return 'Indication16';
-                case 'Rejected':
-                    return 'Error';
-                case 'Resolved':
-                    return 'Success';
-                case 'In Process':
-                    return 'Warning'
-                default:
-                    return 'None';
-            }
-        },
+      ComplaintStatusState(sState) {
+        switch (sState) {
+          case "Open":
+            return "Information";
+          case "Closed":
+            return "Indication16";
+          case "Rejected":
+            return "Error";
+          case "Resolved":
+            return "Success";
+          case "In Process":
+            return "Warning";
+          default:
+            return "None";
+        }
+      },
     };
-
-}, /* bExport= */ true);
+  },
+  /* bExport= */ true
+);
